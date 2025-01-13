f5f91bfeb7aacce49035eeaa6b11df7d32e939bc
RANAIVOSON Nantenaina Claudio <ranaivosonclaudio@gmail.com>
2025-01-13 08:09:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,44 @@
 const express = require("express");
 const path = require("path");
-const { Socket } = require("socket.io");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = require("http").createServer(app);
 
-const io = require("socket.io")(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*", 
+        methods: ["GET", "POST"]
+    }
+});
 
 app.use(express.static(path.join(__dirname + "/public")));
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
-io.on("connection", function (Socket) {
-    Socket.on("newuser", function (user) {
-        io.emit("update", user.username + " has joined the chat."); 
+io.on("connection", function (socket) {
+    console.log("A user connected");
+
+    socket.on("newuser", function (user) {
+        io.emit("update", user.username + " has joined the chat.");
     });
-    Socket.on("exituser", function (username) {
+
+    socket.on("exituser", function (username) {
         io.emit("update", username + " has left the chat.");
     });
-    Socket.on("chat", function (message) {
+
+    socket.on("chat", function (message) {
         io.emit("chat", message);
     });
-    Socket.on("image", function (imageData) {
+
+    socket.on("image", function (imageData) {
         io.emit("image", imageData);
     });
-    Socket.on("error", function (err) {
+
+    socket.on("disconnect", function () {
+        console.log("A user disconnected");
+    });
+
+    socket.on("error", function (err) {
         console.error("Socket error:", err);
     });
 });
